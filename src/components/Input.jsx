b456d0ca98ee11491b7cc948b56ba427f3f41c74
DinoCzar <ryanb_booking@yahoo.com>
2023-11-06 15:59:24 -0800

    Add form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,23 @@
 function Input() {
 	return (
-		<div className='section'>
-			<div className='section'>Input</div>
-			<form>
-				<input type='text' placeholder='name' />
-			</form>
+		<div id='modal'>
+			<fieldset>
+				<legend>Add New Book</legend>
+				<div class='form'>
+					<input type='text' name='name' id='name' placeholder='Name' />
+				</div>
+				<div class='form'>
+					<input type='text' name='phone' id='phone' placeholder='Phone' />
+				</div>
+				<div class='form'>
+					<input type='text' name='city' id='city' placeholder='City' />
+				</div>
+				<div id='submit-button'>
+					<button type='submit' id='submit'>
+						Submit
+					</button>
+				</div>
+			</fieldset>
 		</div>
 	);
 }
