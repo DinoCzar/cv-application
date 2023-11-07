fb6517321e5c51aac3e2c0602f13df712d535f84
DinoCzar <ryanb_booking@yahoo.com>
2023-11-06 23:34:33 -0800

    Add function to display input text in div

--- src/App.jsx
+++ src/App.jsx
@@ -7,52 +7,64 @@ import Button from './components/Button';
 import './styles/App.css';
 
 function App() {
-	const handleButtonClick = (url) => {
-		window.location.href = url;
-	};
+  const [educationValue, setEducationValue] = useState('');
 
-	return (
-		<>
-			<div id='container'>
-				<div id='sidebar'>
-					<div id='view'>
-						<div id='clear'></div>
-						<div id='example'></div>
-					</div>
-					<div id='personal'>
-						<Input
-							legend={'Full Name'}
-							placeholder='Name'
-							id='name'
-							name='name'
-						/>
-						<Input
-							legend={'Phone'}
-							placeholder='Phone'
-							id='phone'
-							name='phone'
-						/>
-						<Input
-							legend={'Address'}
-							placeholder='City, State'
-							id='address'
-							name='address'
-						/>
-					</div>
-					<div id='education'></div>
-					<div id='experience'></div>
-				</div>
-				<div id='cv'>
-					<Personal />
-					<Education />
-					<Experience />
-				</div>
-			</div>
-			<div id='submit-div'>
-				<Button handleClick={handleButtonClick} />
-			</div>
-		</>
-	);
+  const handleButtonClick = (url) => {
+    window.location.href = url;
+  };
+
+  return (
+    <>
+      <div id='container'>
+        <div id='sidebar'>
+          <div id='view'>
+            <div id='clear'></div>
+            <div id='example'></div>
+          </div>
+          <div id='personal'>
+            <Input
+              legend={'Full Name'}
+              placeholder='Name'
+              id='name'
+              name='name'
+            />
+            <Input
+              legend={'Phone'}
+              placeholder='Phone'
+              id='phone'
+              name='phone'
+            />
+            <Input
+              legend={'Address'}
+              placeholder='City, State'
+              id='address'
+              name='address'
+            />
+          </div>
+          <div id='education'>
+            <input
+              type="text"
+              value={educationValue}
+              onChange={(e) => setEducationValue(e.target.value)}
+              placeholder="Enter your education information"
+            />
+          </div>
+          <div id='display-education'>
+            {educationValue}
+          </div>
+          <div id='experience'></div>
+        </div>
+        <div id='cv'>
+          <Personal />
+          <Education />
+          <Experience />
+        </div>
+      </div>
+      <div id='submit-div'>
+        <Button handleClick={handleButtonClick} />
+      </div>
+    </>
+  );
 }
 
 export default App;
