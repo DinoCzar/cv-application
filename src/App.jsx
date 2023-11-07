1028a042e091689cb0f49ea39453178b170c5c84
DinoCzar <ryanb_booking@yahoo.com>
2023-11-07 00:02:33 -0800

    Add display input functionality to input component

--- src/App.jsx
+++ src/App.jsx
@@ -6,68 +6,61 @@ import Button from './components/Button';
 import './styles/App.css';
 
 function App() {
-  const [personalValue, setPersonalValue] = useState('');
+	const [personalValue, setPersonalValue] = useState('');
 
-  const handleButtonClick = (url) => {
-    window.location.href = url;
-  };
+	const handleButtonClick = (url) => {
+		window.location.href = url;
+	};
 
-  // Function to handle changes in the 'Full Name' input
-  const handleFullNameChange = (e) => {
-    setPersonalValue(e.target.value);
-  };
+	const handleFullNameChange = (e) => {
+		setPersonalValue(e.target.value);
+	};
 
-  return (
-    <>
-      <div id='container'>
-        <div id='sidebar'>
-          <div id='view'>
-            <div id='clear'></div>
-            <div id='example'></div>
-          </div>
-          <div id='personal'>
-            <input
-			
-              legend={'Full Name'}
-              placeholder='Name'
-              id='name'
-              name='name'
-    
-
-			  type="text"
-              value={personalValue}
-              onChange={(e) => setPersonalValue(e.target.value)}>
-
-			  </input>
-            <Input
-              legend={'Phone'}
-              placeholder='Phone'
-              id='phone'
-              name='phone'
-            />
-            <Input
-              legend={'Address'}
-              placeholder='City, State'
-              id='address'
-              name='address'
-            />
-          </div>
-          <div id='education'>
-           
-          </div>
-          <div id='experience'></div>
-        </div>
-        <div id='cv'>
-          <div id='personal'>{personalValue}</div>
-          <Education />
-          <Experience />
-        </div>
-      </div>
-      <div id='submit-div'>
-        <Button handleClick={handleButtonClick} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div id='container'>
+				<div id='sidebar'>
+					<div id='view'>
+						<div id='clear'></div>
+						<div id='example'></div>
+					</div>
+					<div id='personal'>
+						<Input
+							legend={'Full Name'}
+							placeholder='Name'
+							id='name'
+							name='name'
+							type='text'
+							value={personalValue}
+							onChange={(e) => setPersonalValue(e.target.value)} />
+					
+						<Input
+							legend={'Phone'}
+							placeholder='Phone'
+							id='phone'
+							name='phone'
+						/>
+						<Input
+							legend={'Address'}
+							placeholder='City, State'
+							id='address'
+							name='address'
+						/>
+					</div>
+					<div id='education'></div>
+					<div id='experience'></div>
+				</div>
+				<div id='cv'>
+					<div id='personal'>{personalValue}</div>
+					<Education />
+					<Experience />
+				</div>
+			</div>
+			<div id='submit-div'>
+				<Button handleClick={handleButtonClick} />
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,17 @@
-function Input({legend, name, placeholder, id}) {
+function Input({legend, name, placeholder, id, value, onChange}) {
     const formInfo = {
         name: name,
         placeholder: placeholder,
         id: id,
+		value: value,
+		onChange: onChange
     };
 
 	return (
 		<div className='user-input'>
 				<legend>{legend}</legend>
 				<div class='form'>
-					<input type='text' name={formInfo.name} id={formInfo.id} placeholder={formInfo.placeholder} />
+					<input type='text' name={formInfo.name} id={formInfo.id} placeholder={formInfo.placeholder} value={formInfo.value} onChange={formInfo.onChange} />
 				</div>
 		</div>
 	);
