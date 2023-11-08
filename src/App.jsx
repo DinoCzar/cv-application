3cf3ef53b1fd94c181edca01037f5a54848fb23f
DinoCzar <ryanb_booking@yahoo.com>
2023-11-08 00:49:23 -0800

    Create other forms for education and experience with logic

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,45 @@ function App() {
 	const [nameValue, setNameValue] = useState('');
 	const [phoneValue, setPhoneValue] = useState('');
 	const [addressValue, setAddressValue] = useState('');
+	const [firstSchoolValue, setFirstSchoolValue] = useState('');
+	const [secondSchoolValue, setSecondSchoolValue] = useState('');
+	const [thirdSchoolValue, setThirdSchoolValue] = useState('');
+	const [firstJobValue, setFirstJobValue] = useState('');
+	const [secondJobValue, setSecondJobValue] = useState('');
+	const [thirdJobValue, setThirdJobValue] = useState('');
 
 	const [displayName, setDisplayName] = useState('');
 	const [displayPhone, setDisplayPhone] = useState('');
 	const [displayAddress, setDisplayAddress] = useState('');
+	const [displayFirstSchool, setDisplayFirstSchool] = useState('');
+	const [displaySecondSchool, setDisplaySecondSchool] = useState('');
+	const [displayThirdSchool, setDisplayThirdSchool] = useState('');
+	const [displayFirstJob, setDisplayFirstJob] = useState('');
+	const [displaySecondJob, setDisplaySecondJob] = useState('');
+	const [displayThirdJob, setDisplayThirdJob] = useState('');
 
 	const submitButtonClick = () => {
 		setNameValue('');
 		setPhoneValue('');
 		setAddressValue('');
+		setFirstSchoolValue('');
+		setSecondSchoolValue('');
+		setThirdSchoolValue('');
+		setFirstJobValue('');
+		setSecondJobValue('');
+		setThirdJobValue('');
 	};
 
 	const editButtonClick = () => {
 		setNameValue(displayName);
 		setPhoneValue(displayPhone);
 		setAddressValue(displayAddress);
+		setFirstSchoolValue(displayFirstSchool);
+		setSecondSchoolValue(displaySecondSchool);
+		setThirdSchoolValue(displayThirdSchool);
+		setFirstJobValue(displayFirstJob);
+		setSecondJobValue(displaySecondJob);
+		setThirdJobValue(displayThirdJob);
 	};
 
 	return (
@@ -68,8 +92,76 @@ function App() {
 							}}
 						/>
 					</div>
-					<div id='education'></div>
-					<div id='experience'></div>
+					<div id='education'>
+						<Input
+							legend={'School 1'}
+							placeholder='School Name'
+							id='first-school'
+							name='first-school'
+							value={firstSchoolValue}
+							onChange={(e) => {
+								setDisplayFirstSchool(e.target.value);
+								setFirstSchoolValue(e.target.value);
+							}}
+						/>
+						<Input
+							legend={'School 2'}
+							placeholder='School Name'
+							id='second-school'
+							name='second-school'
+							value={secondSchoolValue}
+							onChange={(e) => {
+								setDisplaySecondSchool(e.target.value);
+								setSecondSchoolValue(e.target.value);
+							}}
+						/>
+						<Input
+							legend={'School 3'}
+							placeholder='School Name'
+							id='third-school'
+							name='third-school'
+							value={thirdSchoolValue}
+							onChange={(e) => {
+								setThirdSchoolValue(e.target.value);
+								setDisplayThirdSchool(e.target.value);
+							}}
+						/>
+					</div>
+					<div id='experience'>
+						<Input
+							legend={'Work 1'}
+							placeholder='Experience'
+							id='first-job'
+							name='first-job'
+							value={firstJobValue}
+							onChange={(e) => {
+								setFirstJobValue(e.target.value);
+								setDisplayFirstJob(e.target.value);
+							}}
+						/>
+						<Input
+							legend={'Work 2'}
+							placeholder='Experience'
+							id='second-job'
+							name='second-job'
+							value={secondJobValue}
+							onChange={(e) => {
+								setSecondJobValue(e.target.value);
+								setDisplaySecondJob(e.target.value);
+							}}
+						/>
+						<Input
+							legend={'Work 3'}
+							placeholder='Experience'
+							id='third-job'
+							name='third-job'
+							value={thirdJobValue}
+							onChange={(e) => {
+								setThirdJobValue(e.target.value);
+								setDisplayThirdJob(e.target.value);
+							}}
+						/>
+					</div>
 				</div>
 				<div id='cv'>
 					<div id='personal'>
@@ -83,8 +175,28 @@ function App() {
 							{displayAddress}
 						</div>
 					</div>
-					<Education />
-					<Experience />
+					<div id='display-education'>
+						<div className='display'>
+							{displayFirstSchool}
+						</div>
+						<div className='display'>
+							{displaySecondSchool}
+						</div>
+						<div className='display'>
+							{displayThirdSchool}
+						</div>
+					</div>
+					<div id='display-experience'>
+						<div className='display'>
+							{displayFirstJob}
+						</div>
+						<div className='display'>
+							{displaySecondJob}
+						</div>
+						<div className='display'>
+							{displayThirdJob}
+						</div>
+					</div>
 				</div>
 			</div>
 			<div id='submit-div'>
