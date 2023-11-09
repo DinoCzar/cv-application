463cb2593d0e1b29b849076905aa2b9deb9d2ea2
DinoCzar <ryanb_booking@yahoo.com>
2023-11-09 12:32:34 -0800

    Delete input keys

--- src/App.jsx
+++ src/App.jsx
@@ -4,110 +4,116 @@ import Button from './components/Button';
 import './styles/App.css';
 
 const initialValues = {
-  name: '',
-  phone: '',
-  address: '',
-  firstSchool: '',
-  secondSchool: '',
-  thirdSchool: '',
-  firstJob: '',
-  secondJob: '',
-  thirdJob: '',
+	name: '',
+	phone: '',
+	address: '',
+	firstSchool: '',
+	secondSchool: '',
+	thirdSchool: '',
+	firstJob: '',
+	secondJob: '',
+	thirdJob: '',
 };
 
 function App() {
-  const [formValues, setFormValues] = useState({ ...initialValues });
-  const [displayValues, setDisplayValues] = useState({ ...initialValues });
+	const [formValues, setFormValues] = useState({ ...initialValues });
+	const [displayValues, setDisplayValues] = useState({ ...initialValues });
 
-  const handleChange = (key, value) => {
-    setFormValues({ ...formValues, [key]: value });
-    setDisplayValues({ ...displayValues, [key]: value });
-  };
+	const handleChange = (key, value) => {
+		setFormValues({ ...formValues, [key]: value });
+		setDisplayValues({ ...displayValues, [key]: value });
+	};
 
-  const resetForm = () => {
-    setFormValues({ ...initialValues });
-  };
+	const resetForm = () => {
+		setFormValues({ ...initialValues });
+	};
 
-  const resetDisplay = () => {
-    setFormValues({ ...displayValues });
-  };
+	const resetDisplay = () => {
+		setFormValues({ ...displayValues });
+	};
 
-  return (
-    <>
-      <div id='container'>
-        <div id='sidebar'>
-          <div id='view'>
-            <Button handleClick={resetDisplay} text='Edit' />
-          </div>
-          <div id='personal'>
-            {Object.entries(formValues).map(([key, value]) => (
-              <Input
-                key={key}
-                legend={key === 'name' ? 'Full Name' : key}
-                placeholder={key === 'name' ? 'Name' : key}
-                id={key}
-                name={key}
-                value={value}
-                onChange={(e) => handleChange(key, e.target.value)}
-              />
-            ))}
-          </div>
-          <div id='education'>
-            {['firstSchool', 'secondSchool', 'thirdSchool'].map((key) => (
-              <Input
-                key={key}
-                legend={`School ${key === 'firstSchool' ? '1' : key === 'secondSchool' ? '2' : '3'}`}
-                placeholder='School Name'
-                id={key}
-                name={key}
-                value={formValues[key]}
-                onChange={(e) => handleChange(key, e.target.value)}
-              />
-            ))}
-          </div>
-          <div id='experience'>
-            {['firstJob', 'secondJob', 'thirdJob'].map((key) => (
-              <Input
-                key={key}
-                legend={`Work ${key === 'firstJob' ? '1' : key === 'secondJob' ? '2' : '3'}`}
-                placeholder='Experience'
-                id={key}
-                name={key}
-                value={formValues[key]}
-                onChange={(e) => handleChange(key, e.target.value)}
-              />
-            ))}
-          </div>
-        </div>
-        <div id='cv'>
-          <div id='personal'>
-            {Object.entries(displayValues).map(([key, value]) => (
-              <div className='display' key={key} id={`display-${key}`}>
-                {value}
-              </div>
-            ))}
-          </div>
-          <div id='display-education'>
-            {['firstSchool', 'secondSchool', 'thirdSchool'].map((key) => (
-              <div className='display' key={key}>
-                {displayValues[key]}
-              </div>
-            ))}
-          </div>
-          <div id='display-experience'>
-            {['firstJob', 'secondJob', 'thirdJob'].map((key) => (
-              <div className='display' key={key}>
-                {displayValues[key]}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div id='submit-div'>
-        <Button handleClick={resetForm} text='Submit' />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div id='container'>
+				<div id='sidebar'>
+					<div id='view'>
+						<Button handleClick={resetDisplay} text='Edit' />
+					</div>
+					<div id='personal'>
+						{Object.entries(formValues).map(([key, value]) => (
+							<Input
+					
+								legend={key === 'name' ? 'Full Name' : key}
+								placeholder={key === 'name' ? 'Name' : key}
+								id={key}
+								name={key}
+								value={value}
+								onChange={(e) => handleChange(key, e.target.value)}
+							/>
+						))}
+					</div>
+					<div id='education'>
+						{['firstSchool', 'secondSchool', 'thirdSchool'].map((key) => (
+							<Input
+								legend={`School ${
+									key === 'firstSchool'
+										? '1'
+										: key === 'secondSchool'
+										? '2'
+										: '3'
+								}`}
+								placeholder='School Name'
+								id={key}
+								name={key}
+								value={formValues[key]}
+								onChange={(e) => handleChange(key, e.target.value)}
+							/>
+						))}
+					</div>
+					<div id='experience'>
+						{['firstJob', 'secondJob', 'thirdJob'].map((key) => (
+							<Input
+								legend={`Work ${
+									key === 'firstJob' ? '1' : key === 'secondJob' ? '2' : '3'
+								}`}
+								placeholder='Experience'
+								id={key}
+								name={key}
+								value={formValues[key]}
+								onChange={(e) => handleChange(key, e.target.value)}
+							/>
+						))}
+					</div>
+				</div>
+				<div id='cv'>
+					<div id='personal'>
+						{Object.entries(displayValues).map(([key, value]) => (
+							<div className='display' key={key} id={`display-${key}`}>
+								{value}
+							</div>
+						))}
+					</div>
+					<div id='display-education'>
+						{['firstSchool', 'secondSchool', 'thirdSchool'].map((key) => (
+							<div className='display' key={key}>
+								{displayValues[key]}
+							</div>
+						))}
+					</div>
+					<div id='display-experience'>
+						{['firstJob', 'secondJob', 'thirdJob'].map((key) => (
+							<div className='display' key={key}>
+								{displayValues[key]}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+			<div id='submit-div'>
+				<Button handleClick={resetForm} text='Submit' />
+			</div>
+		</>
+	);
 }
 
 export default App;
